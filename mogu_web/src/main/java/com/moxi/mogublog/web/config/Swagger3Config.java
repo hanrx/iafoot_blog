a7af56e805e04c4e9d967340e743c1b9090c7e3d
hanrx <1170807870@qq.com>
2020-12-09 14:02:42 +0800

    添加注释，删除无效代码

--- mogu_web/src/main/java/com/moxi/mogublog/web/config/Swagger3Config.java
+++ mogu_web/src/main/java/com/moxi/mogublog/web/config/Swagger3Config.java
@@ -25,7 +25,7 @@ public class Swagger3Config {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("蘑菇博客Web接口文档")
+                .title("iAfoot博客Web接口文档")
                 .description("简单优雅的restful风格")
                 .version("1.0")
                 .build();
